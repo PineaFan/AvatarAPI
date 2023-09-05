c92b7da8398b4c984b86cd1957f99cb5175c66c9
pineafan <ash@pinea.dev>
2023-09-05 16:23:14 +0100

    New frames, update to about, and caching

--- endpoints/about.py
+++ endpoints/about.py
@@ -1,13 +1,21 @@
+import os
+
 path = ""
 
 def main(*args, **kwargs):
-    return """
+    # Load all filenames in assets/frames
+    frames = [f'`{x.split(".")[0].lower()}`' for x in os.listdir('assets/frames')]
+    frame_list = ", ".join(frames)
+    # Replace the final "," with ", or"
+    frame_list = ', or'.join(frame_list.rsplit(',', 1))
+
+    return f"""
     # Avatar endpoints
     Use either `avatar.png` or `avatar.svg` to generate an avatar, depending on the format you want.
 
     ## Parameters
     - colour: The colour of the avatar. Can be one of `purple`, `red`, `black`, `green`, `blue` or `yellow`. Defaults to `purple`.
-    - frame: The frame to use. Omit this for no frame. One of `gay`, `trans`, `enby` or `ace`. Defaults to `none`.
+    - frame: The frame to use. Omit this for no frame. One of {frame_list}. Defaults to `none`.
       - This can only be used with the `circle` shape, otherwise the frame will be ignored.
     - shape: The shape of the avatar. One of `circle` or `square`. Defaults to `circle`.
     - gradient_start: The start colour of the gradient. Defaults to the value needed for colour.
--- src/generate.py
+++ src/generate.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 import numpy as np
 import cv2
 import cairosvg
+import os
 
 
 colour_definitions = {
@@ -50,6 +51,38 @@ def set_pineapple_colour(base_image, colour):
     return base_image
 
 
+def correct_hex(code):
+    code = str(code).strip("#")
+    code = "".join([char for char in code.upper() if char in "0123456789ABCDEF"])
+    if not code:
+        return "000000"
+
+    if len(code) > 6:
+        code = code[:6]
+    if len(code) == 6:
+        return code
+
+    if len(code) == 1:
+        return code * 6
+    if len(code) == 2:
+        return code * 3
+
+    # If 4 characters, assume the last character is the alpha channel, and strip it
+    if len(code) == 4:
+        code = code[:3]
+    if len(code) == 3:
+        return "".join([char * 2 for char in code])
+
+    # For 5 characters, it is probable that a leading zero was omitted
+    if len(code) == 5:
+        code = "0" + code
+
+
+def save_to_cache(image, name):
+    # Save the image to the cache folder
+    with open(f"cache/{name}", "wb") as f:
+        f.write(image)
+
 
 def main(
     output_format="svg",  # svg, 512 or 2048
@@ -68,13 +101,20 @@ def main(
         frame = None
     if frame and shape != "circle":
         frame = None
+    if gradient_start and gradient_end:
+        gradient_start, gradient_end = gradient_end, gradient_start
+    else:
+        gradient_start, gradient_end = colour_definitions[colour]
+    gradient_start, gradient_end = correct_hex(gradient_start), correct_hex(gradient_end)
+
+    full_name = f"{output_format}-{gradient_start}{gradient_end}-{frame}{shape}{pineapple_colour}.{'svg' if output_format == 'svg' else 'png'}"
+    # Check if the image has already been generated. If it has been, it will be in the cache folder
+    if os.path.exists(f"cache/{full_name}"):
+        return open(f"cache/{full_name}", "rb").read()
 
     base_image = ET.parse(f"assets/base.svg").getroot()
 
-    if gradient_start and gradient_end:
-        base_image = set_gradient(base_image, gradient_end, gradient_start)
-    else:
-        base_image = set_gradient(base_image, *colour_definitions[colour])
+    base_image = set_gradient(base_image, gradient_start, gradient_end)
 
     if pineapple_colour:
         base_image = set_pineapple_colour(base_image, pineapple_colour)
@@ -87,7 +127,9 @@ def main(
 
 
     if output_format == "svg":
-        return ET.tostring(base_image)
+        generated = ET.tostring(base_image)
+        save_to_cache(generated, full_name)
+        return generated
 
     # Convert the SVG to a PNG using cairosvg
     image = cairosvg.svg2png(bytestring=ET.tostring(base_image))
@@ -100,5 +142,8 @@ def main(
         image = cv2.resize(image, (2048, 2048), interpolation=cv2.INTER_AREA)
     # Convert the image back to a PNG
     image = cv2.imencode(".png", image)[1].tostring()
+
+    save_to_cache(image, full_name)
+
     # Return the PNG as a bytestring
     return image
