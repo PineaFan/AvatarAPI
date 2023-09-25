7f55195a7dc0c785655358d9b1e578715df5242e
pineafan <ash@pinea.dev>
2023-09-25 16:51:16 +0100

    Bug

--- endpoints/avatar_png.py
+++ endpoints/avatar_png.py
@@ -4,4 +4,6 @@ path = "avatar.png"
 
 def main(*args, **kwargs):
     output_format = kwargs.get("output_format", "2048")
+    if "output_format" in kwargs:
+        del kwargs["output_format"]
     return generate.main(output_format, **kwargs)
